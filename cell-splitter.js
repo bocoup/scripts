14b5553b5ca02325ff8c1bc6769dbf4c40f14256
Mike Pennisi <mike@mikepennisi.com>
2020-04-08 15:27:39 -0400

    fixup! Introduce "cell splitter" script

--- cell-splitter.js
+++ cell-splitter.js
@@ -1,64 +1,63 @@
 /**
- * Airtable Field Split Script
+ * Airtable field split script
  *
- * For each record in a given Airtable Base, read the value of one cell,
+ * For each record in a given Airtable base, read the value of one cell,
  * interpret that value as a space-separated list (e.g. the value "a b c"
  * becomes "a", "b", and "c"), and store each value in a corresponding
  * destination cell.
  *
  * How to adapt this script
  *
- * 1. Replace the value assigned to the `tableName` constant with the name of
- *    the table in your Base
- * 2. Replace the value of the `sourceName` constant with the field which
+ * 1. Replace the value assigned to the `tableName` variable with the name of
+ *    the table in your base
+ * 2. Replace the value of the `sourceFieldName` variable with the field which
  *    contains the source value (that is: the value to be split).
- * 3. Replace the value of the `destinationNames` constant with a JavaScript
+ * 3. Replace the value of the `destinationFieldNames` variable with a JavaScript
  *    array of string values, each describing the name of a field to store one
  *    of the split values.
  */
-const tableName = 'Split Demo';
-const sourceName = 'My Things';
-const destinationNames = ['Thing 1', 'Thing 2', 'Thing 3'];
+let tableName = 'Split Demo';
+let sourceFieldName = 'My Things';
+let destinationFieldNames = ['Thing 1', 'Thing 2', 'Thing 3'];
 
 // You shouldn't need to modify the code following this line to use this
 // script.
 
 async function main() {
     if (typeof tableName !== 'string') {
-        throw new Error('The `tableName` constant must be a JavaScript string value.');
+        throw new Error('The `tableName` variable must be a string.');
     }
-    if (typeof sourceName !== 'string') {
-        throw new Error('The `sourceName` constant must be a JavaScript string value.');
+    if (typeof sourceFieldName !== 'string') {
+        throw new Error('The `sourceFieldName` variable must be a string.');
     }
-    if (!Array.isArray(destinationNames)) {
-        throw new Error('The `destinationNames` constant must be a JavaScript array value.');
+    if (!Array.isArray(destinationFieldNames)) {
+        throw new Error('The `destinationFieldNames` variable must be an array.');
     }
-    if (!destinationNames.length) {
-        throw new Error('The `destinationNames` array must have at least one element.');
+    if (!destinationFieldNames.length) {
+        throw new Error('The `destinationFieldNames` array must have at least one element.');
     }
-    if (!destinationNames.every((name) => typeof name === 'string')) {
-        throw new Error(
-            'Every element of the `destinationNames` array must be a JavaScript string value'
-        );
+    if (!destinationFieldNames.every((name) => typeof name === 'string')) {
+        throw new Error('Every element in the `destinationFieldNames` array must be a string.');
     }
 
     let table = base.getTable(tableName);
-    let result = await table.selectRecordsAsync();
+    let query = await table.selectRecordsAsync();
 
-    for (let record of result.records) {
-        let values = (record.getCellValueAsString(sourceName) || '').split(/\s+/);
+    for (let record of query.records) {
+        let values = (record.getCellValueAsString(sourceFieldName) || '').split(/\s+/);
 
-        if (values.length > destinationNames.length) {
+        if (values.length > destinationFieldNames.length) {
             output.markdown(
                 `**WARNING**: encountered ${values.length} values, but only ` +
-                    `${destinationNames.length} destination fields are available. ` +
+                    `${destinationFieldNames.length} destination fields are available. ` +
                     `(values: ${values})`
             );
 
-            values.length = destinationNames.length;
+            // Remove the extraneous values from the array
+            values.length = destinationFieldNames.length;
         }
 
-        // Create an object suitable for use in `updateRecordAsync`. It keys
+        // Create an object suitable for use in `updateRecordAsync`. Its keys
         // are the names of the "destination" fields as specified at the
         // beginning of this scripts, and its values are the corresponding
         // values from the "source" field.
@@ -72,12 +71,13 @@ async function main() {
         //       "second": "bar",
         //       "third": ""
         //     }
-        let toUpdate = destinationNames.reduce((memo, name) => {
-            memo[name] = values.shift();
-            return memo;
-        }, {});
+        let fields = {};
+        for (let i = 0; i < destinationFieldNames.length; i++) {
+            let fieldName = destinationFieldNames[i];
+            fields[fieldName] = values[i];
+        }
 
-        await table.updateRecordAsync(record, toUpdate);
+        await table.updateRecordAsync(record, fields);
     }
 }
 
@@ -86,46 +86,3 @@ try {
 } catch (error) {
     output.markdown(`**ERROR**: ${error}`);
 }
-
-/**
- * Quality assurance testing plan:
- *
- * 1. Create a table named "Split QA" with the following text fields: "source",
- *    "d1", "d2", and "d3"
- * 2. Populate the table with the following records:
- *
- *    source    | d1 | d2 | d3
- *    ----------|----|----|---
- *              | x  | x  | x
- *    abcde     | x  | x  | x
- *    a bcde    | x  | x  | x
- *    a b cde   | x  | x  | x
- *    a b c de  | x  | x  | x
- *    a b c d e | x  | x  | x
- *
- * 3. Install the Scripting block
- * 4. Insert this source code into the scripting block
- * 5. Modify the `tableName` constant to be `"Split QA"`
- * 6. Update the `sourceName` constant to be `"source"`
- * 7. Update the `destinationNames` constant to be `["d1", "d2", "d3"]`
- * 8. Run the script
- *
- * Expected: every record should be updated, and a message should be printed to
- * the scripting block's output container.
- *
- * Table:
- *
- *    source    | d1    | d2   | d3
- *    ----------|-------|------|---
- *              |       |      |
- *    abcde     | abcde |      |
- *    a bcde    | a     | bcde |
- *    a b cde   | a     | b    | cde
- *    a b c de  | a     | b    | c
- *    a b c d e | a     | b    | c
- *
- * Message:
- *
- *     WARNING: encountered 4 values, but only 3 destination fields are available. (values: a,b,c,de)
- *     WARNING: encountered 5 values, but only 3 destination fields are available. (values: a,b,c,d,e)
- */
