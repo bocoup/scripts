f3f766ed242ed2e3ad78332e28b0a6eabda46db6
Michael "Z" Goddard <mzgoddard@gmail.com>
2020-05-08 11:51:59 -0400

    invert fieldMap and move output out of renderProgress

--- fill-company-logo.js
+++ fill-company-logo.js
@@ -1,50 +1,39 @@
 /**
  * Fill in company websites and logos script
  *
- * With a text field with names of companies, for records that have an empty
- * company website field or an empty company logo field, this will request
- * suggestions from Clearbit's Autocomplete API to fill the website or logo
- * fields.
+ * With a text field with names of companies, records that have an empty company
+ * website field or an empty company logo field, this will request suggestions
+ * from Clearbit's Autocomplete API to fill the website or logo fields.
  *
  * https://clearbit.com/docs#autocomplete-api
  *
  * How to use with your Airtable base
  *
  * 1. Set the tableName variable to the table you want it to check.
- * 2. Update the keys in the fieldMap variable to names of fields in the table
- *    that are single line text or attachment fields. You do not need to
- *    change the values. The values are the properties in the responses this
- *    gets from the Clearbit API. The domain and logo properties are optional.
- *    You can use one by commenting out the other. Or you can use both. Name,
- *    though, is required and cannot be commented out.
+ * 2. Update the values in the fieldNameByClearbitProperty variable to names of
+ *    fields in the table that are single line text or attachment fields. You do
+ *    not need to change the keys. The keys are the properties in the responses
+ *    this script gets from the Clearbit API. The name field is required, but
+ *    the domain and logo fields are optional. If you don't need one of them,
+ *    you can comment it out.
  */
 
 let tableName = 'Clients';
 // A map of Airtable field name keys to Clearbit Autocomplete API response
 // properties.
-let fieldMap = {
+let fieldNameByClearbitProperty = {
     // A single line text field
-    'Name': 'name',
+    'name': 'Name',
     // A single line text field
-    'Website': 'domain',
+    'domain': 'Website',
     // An attachments field
-    'Attachments': 'logo',
+    'logo': 'Attachments',
 };
 
 // After this line is the script logic. To use this script as-is, you do not need
 // to edit anything after this point.
 
-function fieldForParam(fieldMap, param) {
-    for (let fieldName in fieldMap) {
-        let clearbitProperty = fieldMap[fieldName];
-        if (clearbitProperty === param) {
-            return fieldName;
-        }
-    }
-    return null;
-}
-
-// True when given an attachment field with at least one attachement.
+// True when given an attachment cell with at least one attachment.
 function includesAttachment(cell) {
     return cell && cell.length > 0;
 }
@@ -52,27 +41,13 @@ function includesAttachment(cell) {
 let progressHeader = `# Fill in company websites and logos
 
 [Logos provided by Clearbit](https://clearbit.com)
-
 `;
 
-let lastRender = 0;
+let nameFieldName = fieldNameByClearbitProperty.name;
+let domainFieldName = fieldNameByClearbitProperty.domain;
+let logoFieldName = fieldNameByClearbitProperty.logo;
 
-async function renderProgress(stage, body) {
-    // Throttle renders to 2 times a second or the last stage.
-    if (stage !== 'done' && Date.now() - lastRender < 500) {
-        return;
-    }
-    lastRender = Date.now();
-
-    await output.clear();
-    await output.markdown(progressHeader + body);
-}
-
-let nameFieldName = fieldForParam(fieldMap, 'name');
-let domainFieldName = fieldForParam(fieldMap, 'domain');
-let logoFieldName = fieldForParam(fieldMap, 'logo');
-
-let fieldNames = Object.keys(fieldMap); 
+let fieldNames = Object.values(fieldNameByClearbitProperty); 
 
 let table = base.getTable(tableName);
 
@@ -81,24 +56,20 @@ let totalSelected = 0;
 let totalFiltered = 0;
 let totalSuggested = 0;
 
-await renderProgress(
-    'selecting records',
-    `Selecting records from "${tableName}" ...  
+await output.markdown(
+    `${progressHeader}
+Selecting records from "${tableName}" ...  
 `,
 );
 
+// Pass in the fields parameter to only load data from the fields we need.
 let allRecords = await table.selectRecordsAsync({
     fields: fieldNames,
 });
 
 totalSelected = allRecords.records.length;
 
-await renderProgress(
-    'filtering records',
-    `Filtering ${totalSelected} records from "${tableName}" ...  
-`,
-);
-
+// Filter out records that aren't eligible for autocomplete.
 let records = allRecords.records.filter(record => (
     // A record must have its name field set.
     record.getCellValue(nameFieldName) &&
@@ -108,7 +79,7 @@ let records = allRecords.records.filter(record => (
             domainFieldName && !record.getCellValue(domainFieldName) ||
             !domainFieldName
         ) ||
-        // If searching for logo domains, the logo field must be empty.
+        // If searching for logos, the logo field must be empty.
         (
             logoFieldName && !includesAttachment(record.getCellValue(logoFieldName)) ||
             !logoFieldName
@@ -120,6 +91,10 @@ totalFiltered = records.length;
 
 recordNames = records.map(record => record.getCellValue(nameFieldName));
 
+await output.markdown(
+    `Filtered ${totalSelected} records down to ${totalFiltered} records.`
+);
+
 // Create a list of record updates. After all the updates are in the list we'll
 // submit them in batches to the table.
 let updates = [];
@@ -127,11 +102,8 @@ let updates = [];
 for (let i = 0; i < records.length; i++) {
     let record = records[i];
 
-    await renderProgress(
-        'suggesting',
-        `Filtered ${totalSelected} records down to ${totalFiltered} records.  
-Fetching suggestion for "${recordNames[i]}" (${i + 1} of ${totalFiltered}) from [Clearbit].  
-`,
+    await output.markdown(
+        `Fetching suggestion for "${recordNames[i]}" (${i + 1} of ${totalFiltered}) from [Clearbit].`
     );
 
     // Request info for records one at a time. Clearbit has a request limit.
@@ -157,8 +129,6 @@ Fetching suggestion for "${recordNames[i]}" (${i + 1} of ${totalFiltered}) from 
         }];
     }
 
-    if (Object.keys(fields).length === 0) continue;
-
     updates.push({
         id: record.id,
         fields,
@@ -171,9 +141,10 @@ for (let i = 0; i < updates.length; i += updateLimit) {
     await table.updateRecordsAsync(updates.slice(i, i + updateLimit));
 }
 
-await renderProgress(
-    'done',
-    `Filtered ${totalSelected} records down to ${totalFiltered} records.  
+await output.clear();
+await output.markdown(
+    `${progressHeader}
+Filtered ${totalSelected} records down to ${totalFiltered} records.  
 Fetched info for ${totalFiltered} records.  
 Found suggestions for ${totalSuggested} out of ${totalFiltered} records.  
 `,
